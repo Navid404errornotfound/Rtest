6c482582c92a4d6725dc24f9541005f977bca3b4
Navid404errornotfound <96200426+Navid404errornotfound@users.noreply.github.com>
2024-09-10 19:18:36 +0330

    Update first.R

--- first.R
+++ first.R
@@ -1,1 +1,2 @@
 vless://Telegram-ipV2Ray-boy@www.speedtest.net:80?path=%2FTelegram-ipV2Ray%3Fed%3D2048&security=none&encryption=none&host=c.1.www.speedtest.net.zula.ir.zemestanil.iR.&type=httpupgrade#HTTP1-Config
+vless://9fbec4fe-0475-4566-bf90-e56913201912@esale.ikco.ir:2083?path=%2F&security=tls&encryption=none&host=nl.snappfoodi.ir&type=ws&sni=nl.snappfoodi.ir#%F0%9F%87%B3%F0%9F%87%B1+H7+mci+mtn
